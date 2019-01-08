cd47daeedae785aca028fc21002c0b83065e5438
skuznet <azon.sk@gmail.com>
2019-01-08 20:42:47 +0300

    add soap

--- rest/src/main/java/io/khasang/auction/controller/AppController.java
+++ rest/src/main/java/io/khasang/auction/controller/AppController.java
@@ -2,6 +2,7 @@ package io.khasang.auction.controller;
 
 import io.khasang.auction.model.MessageService;
 import io.khasang.auction.service.CreateTable;
+import io.khasang.auction.util.CheckText;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -9,20 +10,28 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class AppController {
     @Autowired
     @Qualifier("messageServiceCustom")
     private MessageService messageService;
-
+    @Autowired
+    private CheckText checkText;
     @Autowired
     private CreateTable createTable;
 
     // http://localhost:8080/
     @RequestMapping("/")
+    public String getHelloPage(Model model) {
+        model.addAttribute("name", messageService.getMessage());
+        return "hello";
+    }
+
+    @RequestMapping("/info")
     public String getHelloPage() {
-        return "index";
+        return "cat";
     }
 
     @RequestMapping("/create")
@@ -53,5 +62,11 @@ public class AppController {
         model.addAttribute("password", password);
         model.addAttribute("encodePassword", new BCryptPasswordEncoder().encode(password));
         return "password";
+    }
+
+    @RequestMapping(value = "/check/{text}")
+    @ResponseBody
+    public String checkText(@PathVariable("text") String text) {
+        return checkText.checkWord(text);
     }
 }
