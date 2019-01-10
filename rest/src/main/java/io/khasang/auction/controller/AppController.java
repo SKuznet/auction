c6fb3115142c3317bf589c18d5f1674cf8b6759b
kisin <wolf_joker@inbox.ru>
2019-01-10 22:55:10 +0300

    Add lk page and Log Out form

--- rest/src/main/java/io/khasang/auction/controller/AppController.java
+++ rest/src/main/java/io/khasang/auction/controller/AppController.java
@@ -47,7 +47,12 @@ public class AppController {
     }
 
     @RequestMapping("/registration")
-    public String getRegistrationPage(Model model) {
+    public String getRegistrationPage() {
+        return null;
+    }
+
+    @RequestMapping("/lk")
+    public String getLkPage() {
         return null;
     }
 
